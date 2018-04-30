0738b220e352f7c9a49d0c58e4bb0c269bbf8817
CS70 Cricket <syasunaga@cs.hmc.edu>
2018-04-30 15:49:20 -0700

    fixed structore/some debugging

--- Classification/mat2python.py
+++ Classification/mat2python.py
@@ -7,12 +7,12 @@ import sklearn.preprocessing as prep
 def data():
     # High valence data
     X_high = np.array(scipy.io.loadmat('X_high.mat')['full_list'])
-    X_high.reshape(len(X_high),5*11*32)
+    X_high.reshape(len(X_high),numFeatures(X_high.shape))
     y_high = np.ones(len(X_high))
 
     # Low valence data
     X_low  = np.array(scipy.io.loadmat('X_low.mat')['full_list'])
-    X_low.reshape(len(X_high),5*11*32)
+    X_low.reshape(len(X_high),numFeatures(X_high.shape))
     y_low = np.zeros(len(X_low))
 
     X = np.stack(X_high,X_low)
@@ -20,7 +20,15 @@ def data():
 
     return X,y
 
-
+def numFeatures(shape):
+    '''
+    given shapee of array, calculate how many features there are. 
+    (multiply all of the dimensions except the 1st)
+    '''
+    accumulator = 1
+    for i in range(len(shape)-1):
+        accumulator*=shape[i+1]
+    return accumulator
 def shuffleData(data, label):
     random_data,random_label = sku.shuffle(data,label)
     # print(random_data.shape)
--- generate_features_basic.m
+++
@@ -1,6 +0,0 @@
-%% generate_features
-% Code to generate the features that we put for the 
-chan_index = []
-freq_index = []
-time_index = []
-
