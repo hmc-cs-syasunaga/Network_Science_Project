8ec2c4f6d1db887f84808f7d1d0054395037458d
CS70 Cricket <syasunaga@cs.hmc.edu>
2018-04-29 17:04:03 -0700

    plot some variances

--- data_exploration.m
+++ data_exploration.m
@@ -1,6 +1,7 @@
 
 %% Make nicely formatted data 
 %% 
+clear
 pat_list = []; %(pat,chan, time, freq)
 
 % data_folder = '/Users/macbookpro/Documents/Network_Science_Data/data_preprocessed_matlab';
@@ -9,13 +10,16 @@ pat_list = []; %(pat,chan, time, freq)
 
 for pat = 32 % For each participants %TODO: fix pat with more data
     filename = sprintf('TRFDOut_AllPowerBins_ppt%02d.mat',pat);
-    load(filename,'data')
+    field = sprintf('TRFDOut_AllPowerBins_ppt%02d',pat);
+    data  = load(filename);
+    data = data.(field);
     chan_list = []; % (chan, time, freq)
     for chan = 1:32 % For each channel
-        bins = data.data; %TODO: change according to the structure
+        bins = cell2mat(data(chan)); %TODO: change according to the structure
+        disp(size(bins))
         freq_list = []; % (time, freq);
         for f = 1:5
-            freq_list(:,f) =  mean(bins(f,:,:),2);
+            freq_list(:,f) =  mean(bins(f,2:end,:),2);
             disp(size(freq_list))
         end
         chan_list(chan,:,:) = freq_list;
@@ -23,8 +27,30 @@ for pat = 32 % For each participants %TODO: fix pat with more data
     pat_list(1,:,:,:) = chan_list; %TODO: fix here wth more data
 end
 
-% pat_list = reshape(pat_list, (1,32,5,12)
-disp(size(pat_list))
 
+%% Plot participant variance
+% --> Do we need to normalize??
+% 
+figure(1)
+plot(mean(mean(mean(pat_list,2),3),4))
+title('participants')
 
-%% Do plottng work with pat _list
+%% Plot channel Variance
+% 
+figure(2)
+values = mean(mean(mean(pat_list,1),3),4);
+values = reshape(values,[1,size(values,2)]);
+plot(values)
+title('channels')
+%% Plot Frequency Variance
+figure(3)
+values = mean(mean(mean(pat_list,1),2),3);
+values = reshape(values,[1,size(values,4)]);
+plot(values)
+title('frequencies')
+%% Plot time variance
+figure(4)
+values = mean(mean(mean(pat_list,1),2),4);
+values = reshape(values,[1,size(values,3)]);
+plot(values)
+title('Times')
